a5ce029123931085aac70cc05612482804983f22
Tanya <fullstackon@gmail.com>
2020-06-24 19:05:01 +0300

    HW8

---
+++ build/precache-manifest.a72b8f8d3821c33e24a83be9ca74d5ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7992363529bbbc0bb4e9d48c4bc018",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "740cda693b22ea2dd70e",
+    "url": "/goit-react-hw-08-phonebook/static/js/0.fd6928c6.chunk.js"
+  },
+  {
+    "revision": "e58d77366c7a2acb69dc",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.267cad91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.267cad91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38edbdcbcbdb6bbd7dd2",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.635a1fd0.chunk.js"
+  },
+  {
+    "revision": "8aea106531b543e7c1b5",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.a9560945.chunk.js"
+  },
+  {
+    "revision": "5b7d197435b164e57bb3",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.5bd50110.chunk.js"
+  },
+  {
+    "revision": "6102489036acaf48d19a",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.b71e11bd.chunk.js"
+  },
+  {
+    "revision": "81e673cfa1e744fe564d",
+    "url": "/goit-react-hw-08-phonebook/static/js/8.e7a19592.chunk.js"
+  },
+  {
+    "revision": "7f086133b39ef418a329",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.727855a7.chunk.js"
+  },
+  {
+    "revision": "666d6b3b4f4406f2f7a4",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.8105bdf7.js"
+  }
+]);
